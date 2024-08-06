d9d975ca93172944f16264e08c56c21f58b538e1
Bhuvana-1500 <bhuvanamungara15@gmail.com>
2024-08-06 13:40:52 +0530

    Updating file via API

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = "7572a8a8-2860-4fdb-90db-3e7a00753aa9"
+  subscription_id = "13ba43d9-3859-4c70-9f8d-182debaa038b"
   client_id       = "0d26f5b0-1c83-4b0b-af8d-ac3dbf8476cf"
   client_secret   = "SRS8Q~smnO~sFO2sykkNIJSTY.r4k-maLFt_caZU"
   tenant_id       = "30bf9f37-d550-4878-9494-1041656caf27"
@@ -17,7 +17,7 @@ provider "azurerm" {
 
 provider "azurerm" {
   alias           = "provider0"
-  subscription_id = "7572a8a8-2860-4fdb-90db-3e7a00753aa9"
+  subscription_id = "13ba43d9-3859-4c70-9f8d-182debaa038b"
   client_id       = "0d26f5b0-1c83-4b0b-af8d-ac3dbf8476cf"
   client_secret   = "SRS8Q~smnO~sFO2sykkNIJSTY.r4k-maLFt_caZU"
   tenant_id       = "30bf9f37-d550-4878-9494-1041656caf27"
@@ -28,7 +28,7 @@ resource "azurerm_management_group" "mgmt1" {
   name          = "mgmt1"
   display_name   = "mgmt1"
   subscription_ids = [
-    "7572a8a8-2860-4fdb-90db-3e7a00753aa9"
+    "13ba43d9-3859-4c70-9f8d-182debaa038b"
   ]
   provider = azurerm.provider0
 }
@@ -65,7 +65,7 @@ resource "azurerm_subnet" "sub1" {
 
 resource "azurerm_virtual_network_peering" "vnet1_to_vnet2" {
   provider = azurerm.provider0
-  name                = "hub-to-spoke1"
+  name                = "hub-to-spoke11"
   resource_group_name = azurerm_virtual_network.vnet1.resource_group_name
   virtual_network_name = "vnet1"
   remote_virtual_network_id = azurerm_virtual_network.vnet2.id
@@ -77,7 +77,7 @@ resource "azurerm_virtual_network_peering" "vnet1_to_vnet2" {
 
 resource "azurerm_virtual_network_peering" "vnet2_to_vnet1" {
   provider = azurerm.provider0
-  name                = "spoke-to-hub1"
+  name                = "spoke-to-hub11"
   resource_group_name = azurerm_virtual_network.vnet2.resource_group_name
   virtual_network_name = "vnet2"
   remote_virtual_network_id = azurerm_virtual_network.vnet1.id
@@ -87,6 +87,12 @@ resource "azurerm_virtual_network_peering" "vnet2_to_vnet1" {
   use_remote_gateways = false
 }
 
+resource "azurerm_role_assignment" "example0" {
+  scope                = azurerm_management_group.mgmt1.id
+  role_definition_name = "Owner"
+  principal_id         = "200ba991-de6c-43f2-89c7-3082c59f39a7"
+}
+
 resource "azurerm_management_group_policy_assignment" "policyassignment0" {
   for_each = { for p in csvdecode(file("${path.module}/Policy.csv")): p.displayname => p }
   
@@ -94,11 +100,5 @@ resource "azurerm_management_group_policy_assignment" "policyassignment0" {
   display_name          = each.value.displayname
   policy_definition_id  = each.value.policyid
   management_group_id   = azurerm_management_group.mgmt1.id
-}
-
-resource "azurerm_role_assignment" "example0" {
-  scope                = azurerm_management_group.mgmt1.id
-  role_definition_name = "Owner"
-  principal_id         = "200ba991-de6c-43f2-89c7-3082c59f39a7"
 }
 
